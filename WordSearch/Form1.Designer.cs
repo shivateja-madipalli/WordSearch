bbf12c7d0fd4eb012672a05e121be400093150ae
Shivateja Madipalli <madipallishivateja@gmail.com>
2015-08-23 03:44:20 -0700

    Updates
    Actual Files are added

---
+++ WordSearch/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WordSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnAddtextbox = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(38, 54);
+            this.txt1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(25, 26);
+            this.txt1.TabIndex = 0;
+            this.txt1.TextChanged += new System.EventHandler(this.txt1_TextChanged);
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(145, 54);
+            this.txt3.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(25, 26);
+            this.txt3.TabIndex = 1;
+            this.txt3.TextChanged += new System.EventHandler(this.txt3_TextChanged);
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(197, 54);
+            this.txt4.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(25, 26);
+            this.txt4.TabIndex = 2;
+            this.txt4.TextChanged += new System.EventHandler(this.txt4_TextChanged);
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(90, 54);
+            this.txt2.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(25, 26);
+            this.txt2.TabIndex = 3;
+            this.txt2.TextChanged += new System.EventHandler(this.txt2_TextChanged);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(289, 157);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 37);
+            this.btnFind.TabIndex = 4;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // btnAddtextbox
+            // 
+            this.btnAddtextbox.Location = new System.Drawing.Point(264, 49);
+            this.btnAddtextbox.Name = "btnAddtextbox";
+            this.btnAddtextbox.Size = new System.Drawing.Size(37, 36);
+            this.btnAddtextbox.TabIndex = 5;
+            this.btnAddtextbox.Text = "+";
+            this.btnAddtextbox.UseVisualStyleBackColor = true;
+            this.btnAddtextbox.Click += new System.EventHandler(this.btnAddtextbox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 206);
+            this.Controls.Add(this.btnAddtextbox);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnAddtextbox;
+    }
+}
+
---
+++ WordSearch/Form1.cs
@@ -0,0 +1,444 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace WordSearch
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FindandSortDictionary();
+        }
+
+        ArrayList AArray, BArray, CArray, DArray, EArray, FArray, GArray, HArray, IArray, JArray, KArray, LArray, MArray, NArray, OArray, PArray, QArray, RArray, SArray, TArray, UArray, VArray, WArray, XArray, YArray, ZArray;
+        ArrayList finalWordArray = new ArrayList();
+        //ArrayList DynamicTextBoxesNames = new ArrayList();
+        String checksum = "";
+        int checkval = 0;
+        int CountforDynamicTextBoxes = 0;
+        int LocationForDynamicTextBoxesX = 15;
+        int LocationForDynamicTextBoxesY = 70;
+        //Char[] Characters;
+
+        List<TextBox> DynamicTextBoxes = new List<TextBox>();
+        private void addCommonWords()
+        {
+            var CommonWords = new List<String> { "the", "are", "can", "let", "she", "his" };
+        }
+
+        private void FindandSortDictionary()
+        {
+            String str = File.ReadAllText(@"E:\Visual Studio Projects\WordSearch\convertcsv1.json");
+            var udemy = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(str);
+            for (int i = 0; i < udemy.Count; i++)
+            {
+                var temp = udemy[i];
+                if (temp["FIELD1"] == (i + 1).ToString())
+                {
+                    SortingWords(temp["FIELD2"]);
+                }
+            }
+        }
+
+        private void SortingWords(string inputWord)
+        {
+            checksum = checksum + "#" + inputWord;
+            checkval++;
+            var DividedWord = inputWord.ToArray();
+            ArrayList array = findWhichArrayList(DividedWord[0]);
+            if (inputWord == "only")
+            {
+                int i = 0;
+            }
+            array.Add(inputWord);
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txt1.Text != "" && txt2.Text != "" && txt3.Text != "" && txt4.Text != "")
+                {
+                    Char[] Characters;
+                    Characters = new Char[4 + DynamicTextBoxes.Count];
+                    Characters[0] = txt1.ToString()[36];
+                    Characters[1] = txt2.ToString()[36];
+                    Characters[2] = txt3.ToString()[36];
+                    Characters[3] = txt4.ToString()[36];
+                    if (DynamicTextBoxes.Count != 0)
+                    {
+                        for (int i = 0; i < DynamicTextBoxes.Count; i++)
+                        {
+                            TextBox txt = DynamicTextBoxes[i];
+                            //str = str + txt.Text;
+                            Characters[i + 4] = txt.ToString()[36];
+                        }
+                    }
+                    FindWords(Characters);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+       private void FindWords(Char[] Characters)
+        {
+            for (int i = 0; i < Characters.Count(); i++)
+            {
+                ArrayList array = findWhichArrayList(Characters[i]);
+                if (array != null)
+                {
+                    for (int j = 0; j < array.Count; j++)
+                    {
+                        if (array[j].ToString().ToCharArray().Count() >= Characters.Count())
+                        {
+                            if (CheckForWord(array[j].ToString(), Characters))
+                            {
+                                if (!finalWordArray.Contains(array[j].ToString()))
+                                    finalWordArray.Add(array[j].ToString());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool CheckForWord(String word, Char[] Characters)
+        {
+            try
+            {
+                StringBuilder myword = new StringBuilder(word);
+                for (int k = 0; k < Characters.Count(); k++)
+                {
+                    if (!word.Contains(Characters[k]))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+
+                        myword[word.IndexOf(Characters[k])] = '1';
+                        word = myword.ToString();
+                        //word.Replace( word.IndexOf(Characters[k]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private ArrayList findWhichArrayList(Char cha)
+        {
+            //if (!Char.IsNumber(cha))
+            //{
+            if (cha == 'A' || cha == 'a')
+            {
+                if (AArray == null)
+                {
+                    AArray = new ArrayList();
+                }
+                return AArray;
+            }
+            else if (cha == 'B' || cha == 'b')
+            {
+                if (BArray == null)
+                {
+                    BArray = new ArrayList();
+                }
+                return BArray;
+            }
+            else if (cha == 'C' || cha == 'c')
+            {
+                if (CArray == null)
+                {
+                    CArray = new ArrayList();
+                }
+                return CArray;
+            }
+            else if (cha == 'D' || cha == 'd')
+            {
+                if (DArray == null)
+                {
+                    DArray = new ArrayList();
+                }
+                return DArray;
+            }
+            else if (cha == 'E' || cha == 'e')
+            {
+                if (EArray == null)
+                {
+                    EArray = new ArrayList();
+                }
+                return EArray;
+            }
+            else if (cha == 'F' || cha == 'f')
+            {
+                if (FArray == null)
+                {
+                    FArray = new ArrayList();
+                }
+                return FArray;
+            }
+            else if (cha == 'G' || cha == 'g')
+            {
+                if (GArray == null)
+                {
+                    GArray = new ArrayList();
+                }
+                return GArray;
+            }
+            else if (cha == 'H' || cha == 'h')
+            {
+                if (HArray == null)
+                {
+                    HArray = new ArrayList();
+                }
+                return HArray;
+            }
+            else if (cha == 'I' || cha == 'i')
+            {
+                if (IArray == null)
+                {
+                    IArray = new ArrayList();
+                }
+                return IArray;
+            }
+            else if (cha == 'J' || cha == 'j')
+            {
+                if (JArray == null)
+                {
+                    JArray = new ArrayList();
+                }
+                return JArray;
+            }
+            else if (cha == 'K' || cha == 'k')
+            {
+                if (KArray == null)
+                {
+                    KArray = new ArrayList();
+                }
+                return KArray;
+            }
+            else if (cha == 'L' || cha == 'l')
+            {
+                if (LArray == null)
+                {
+                    LArray = new ArrayList();
+                }
+                return LArray;
+            }
+            else if (cha == 'M' || cha == 'm')
+            {
+                if (MArray == null)
+                {
+                    MArray = new ArrayList();
+                }
+                return MArray;
+            }
+            else if (cha == 'N' || cha == 'n')
+            {
+                if (NArray == null)
+                {
+                    NArray = new ArrayList();
+                }
+                return NArray;
+            }
+            else if (cha == 'O' || cha == 'o')
+            {
+                if (OArray == null)
+                {
+                    OArray = new ArrayList();
+                }
+                return OArray;
+            }
+            else if (cha == 'P' || cha == 'p')
+            {
+                if (PArray == null)
+                {
+                    PArray = new ArrayList();
+                }
+                return PArray;
+            }
+            else if (cha == 'Q' || cha == 'q')
+            {
+                if (QArray == null)
+                {
+                    QArray = new ArrayList();
+                }
+                return QArray;
+            }
+            else if (cha == 'R' || cha == 'r')
+            {
+                if (RArray == null)
+                {
+                    RArray = new ArrayList();
+                }
+                return RArray;
+            }
+            else if (cha == 'S' || cha == 's')
+            {
+                if (SArray == null)
+                {
+                    SArray = new ArrayList();
+                }
+                return SArray;
+            }
+            else if (cha == 'T' || cha == 't')
+            {
+                if (TArray == null)
+                {
+                    TArray = new ArrayList();
+                }
+                return TArray;
+            }
+            else if (cha == 'U' || cha == 'u')
+            {
+                if (UArray == null)
+                {
+                    UArray = new ArrayList();
+                }
+                return UArray;
+            }
+            else if (cha == 'V' || cha == 'v')
+            {
+                if (VArray == null)
+                {
+                    VArray = new ArrayList();
+                }
+                return VArray;
+            }
+            else if (cha == 'W' || cha == 'w')
+            {
+                if (WArray == null)
+                {
+                    WArray = new ArrayList();
+                }
+                return WArray;
+            }
+            else if (cha == 'X' || cha == 'x')
+            {
+                if (XArray == null)
+                {
+                    XArray = new ArrayList();
+                }
+                return XArray;
+            }
+            else if (cha == 'Y' || cha == 'y')
+            {
+                if (YArray == null)
+                {
+                    YArray = new ArrayList();
+                }
+                return YArray;
+            }
+            else if (cha == 'Z' || cha == 'z')
+            {
+                if (ZArray == null)
+                {
+                    ZArray = new ArrayList();
+                }
+                return ZArray;
+            }
+            //}
+            return null;
+        }
+
+        private void btnAddtextbox_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CountforDynamicTextBoxes++;
+                TextBox txt = new TextBox();
+                txt.Name = "TxtDynamic" + CountforDynamicTextBoxes.ToString();
+                txt.Visible = true;
+                txt.Size = new System.Drawing.Size(25, 26);
+                //txt.Margin = new Thickness(10, 15, 950, 0);
+                txt.Location = new Point(LocationForDynamicTextBoxesX, LocationForDynamicTextBoxesY);
+                LocationForDynamicTextBoxesX = LocationForDynamicTextBoxesX + 52;
+                txt.TextChanged += txt_TextChanged;
+                this.Controls.Add(txt);
+                DynamicTextBoxes.Add(txt);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        //private bool CheckWhetherAreTheyLetters(Char[] Characters)
+        //{
+        //    string s = new string(Characters);
+        //    return (Regex.IsMatch(s, @"^[a-zA-Z]+$"));
+        //}
+
+        void txt_TextChanged(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            CheckForEveryEntry(txt);
+        }
+
+        private void txt3_TextChanged(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            CheckForEveryEntry(txt);
+        }
+
+        private void txt4_TextChanged(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            CheckForEveryEntry(txt);
+        }
+
+        private void txt2_TextChanged(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            CheckForEveryEntry(txt);
+        }
+
+        private void txt1_TextChanged(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            CheckForEveryEntry(txt);
+        }
+
+        private void CheckForEveryEntry(TextBox txt)
+        {
+            //TextBox txt = (TextBox)sender;
+            if (Regex.IsMatch(txt.Text, @"^[a-zA-Z]+$"))
+            {
+                if (txt.Text.Count() > 1)
+                {
+                    txt.Text = txt.Text[0].ToString();
+                }
+            }
+            else
+            {
+                txt.Text = "";
+            }
+        }
+    }
+}
